71ca88a552687724d3c243d33b3fbfa2a93699ed
mgfct_muttley <adricie017@student.polsl.pl>
2016-06-05 10:23:41 +0200

    Dodany plik game_variables

---
+++ Kolonia/Final/game_variables.cpp
@@ -0,0 +1,6 @@
+#include "game_variables.h"
+
+int maxBuildingNumber = 0;					// okresla jakiego typu budynek posiada gracz w najwiekszej liczbie
+int totalMagazinesCapacity = 0;				// calkowita pojemnosc magazynow budynkow
+int usedMagazine = 0;						// wykorzystane miejsce w magazynie ogolnym
+int totalResources = 0;
---
+++ Kolonia/Final/game_variables.h
@@ -0,0 +1,4 @@
+extern int maxBuildingNumber;					// okresla jakiego typu budynek posiada gracz w najwiekszej liczbie
+extern int totalMagazinesCapacity;				// calkowita pojemnosc magazynow budynkow
+extern int usedMagazine;						// wykorzystane miejsce w magazynie ogolnym
+extern int totalResources;
